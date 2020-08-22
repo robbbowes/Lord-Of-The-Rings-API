99bdbf379b5438cea9aa24b4305ac6a32ce631cd
robbbowes <up666724@myport.ac.uk>
2020-08-22 11:49:38 +0200

    + character class , service, controller, response

---
+++ Controllers/CharactersController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharactersController : ControllerBase
+    {
+        private readonly ICharacterService _characterService;
+
+        public CharactersController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCharacter(Character newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
+    }
+}
