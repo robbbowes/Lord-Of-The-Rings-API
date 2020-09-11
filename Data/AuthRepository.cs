a70a5289f47118cded821241bdc848f3f2663c85
robbbowes <up666724@myport.ac.uk>
2020-09-11 15:53:55 +0200

    Add User, AuthRepo and IAuthRepo, begin implem.

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,62 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            if (await UserExists(user.Username))
+            {
+                response.Success = false;
+                response.Message = "User already exists.";
+            }
+            else
+            {
+                CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+
+                response.Data = user.Id;
+            }
+            return response;
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            bool userExists = false;
+            if (await _context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower()))
+            {
+                userExists = true;
+            }
+            return userExists;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
--- Dtos/Character/UpdateCharacterDto.cs
+++ Dtos/Character/UpdateCharacterDto.cs
@@ -4,7 +4,6 @@ namespace dotnet_rpg.Dtos.Character
 {
     public class UpdateCharacterDto
     {
-
         public int Id { get; set; }
         public string Name { get; set; } = "Frodo";
         public int HitPoints { get; set; } = 100;
