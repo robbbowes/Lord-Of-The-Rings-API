610998b69a4d00c0f99bfdd4c84f2a9389d0331a
robbbowes <up666724@myport.ac.uk>
2020-09-26 15:52:17 +0200

    Add Dtos, AddWeapon funct. and controller

---
+++ Services/WeaponService/WeaponService.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.Character;
+using dotnet_rpg.Dtos.Weapon;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Services.WeaponService
+{
+    public class WeaponService : IWeaponService
+    {
+        private readonly DataContext _dataContext;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public WeaponService(DataContext dataContext, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _mapper = mapper;
+            _dataContext = dataContext;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDto>> AddWeapon(AddWeaponDto newWeapon)
+        {
+            ServiceResponse<GetCharacterDto> serviceResponse = new ServiceResponse<GetCharacterDto>();
+            try
+            {
+                Character character = await _dataContext.Characters.FirstOrDefaultAsync(
+                    c => c.Id == newWeapon.CharacterId
+                    && c.User.Id == int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier))
+                );
+                if (character == null)
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = "Character not found.";
+                }
+                else
+                {
+                    Weapon weapon = _mapper.Map<Weapon>(newWeapon);
+                    await _dataContext.Weapons.AddAsync(weapon);
+                    await _dataContext.SaveChangesAsync();
+
+                    serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+                }
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return serviceResponse;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
+using dotnet_rpg.Services.WeaponService;
 
 namespace dotnet_rpg
 {
@@ -38,6 +39,7 @@ namespace dotnet_rpg
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IWeaponService, WeaponService>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
                 options =>
                 {
